f964b6df9424f55aa1da0d71fc9326c832e147c4
giupal <gpaletra@teamdigitale.governo.it>
2018-05-21 17:43:36 +0200

    first commit

--- conf.py
+++ conf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # -- PROJECT Variables ----------------------------------------------------
-settings_project_name = "pagoPA Codici 1_3_1"
+settings_project_name = "Specifiche Attuative dei Codici Identificativi di Versamento, Riversamento e Rendicondazione"
 settings_copyright_copyleft = 'AgID - Team Digitale'
 settings_editor_name = 'AgID - Team Digitale'
 settings_doc_version = 'version: latest'
